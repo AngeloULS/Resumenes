95841a381a6ac3ff61292e9909eeba091bc46f1d
Maycol <mgonzalez41@alumnosuls.cl>
2021-07-14 01:11:32 -0400

    Actualización de index
    + cambio flujo de la pagina
    - se elimino código redundante
    + Se separo CSS de HTML
    + Se utilizo Layuot

--- Web/run.py
+++ Web/run.py
@@ -6,7 +6,6 @@ from transformers import BertTokenizerFast, EncoderDecoderModel
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
-
 def modificaTexto(texto):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     ckpt = 'mrm8488/bert2bert_shared-spanish-finetuned-summarization'
@@ -22,39 +21,32 @@ def modificaTexto(texto):
     return resultado
 
 
-
 @app.route('/', methods=["GET","POST"])
 def index():
-   # if request.method == 'GET':
-      #  if request.form.get('action1') == 'VALUE1':
-           # print('btn 1')
-        
-    return render_template('index.html')
+    if request.method == 'POST':
+        return render_template('index.html')
+    elif request.method == 'GET':
+        return render_template('index.html')
 
-@app.route("/f", methods=['GET', 'POST'])
+
+@app.route("/resumen", methods=['GET', 'POST'])
 def indexx():
     if request.method == 'POST':
         if request.form.get('action1') == 'VALUE1':
             print('btn 1')
             texto = request.form['input']
             print(texto)
-
             resultado =''
             resultado = resultado + modificaTexto(texto)
-            
             flash(resultado)
-        elif  request.form.get('action2') == 'VALUE2':
-            print('btn 2')
-        else:
-            pass # unknown
+            flash(texto)
+
+            return render_template('response.html', noticia = texto, resumen = resultado)
+     
     elif request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html') #index.html
     
-    return render_template("index.html")
-
-
-
-
+    return render_template("response.html") #index.html
 
 if __name__ == "__main__":
     app.run(debug= True)
